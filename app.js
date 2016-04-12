ad3ae2c59928480263a10a5d6c51523dd7de461c
Sylvain Ageneau <ageneau@gmail.com>
2016-04-11 21:09:20 -0300

    Add node app

---
+++ app.js
@@ -0,0 +1,15 @@
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
+
+// Serve up public/ftp folder
+var serve = serveStatic('public')
+
+// Create server
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res)
+  serve(req, res, done)
+})
+
+// Listen
+server.listen(3000)
